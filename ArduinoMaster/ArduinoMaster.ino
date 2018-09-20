430b7a5cacb2a59fa53512b76e4b828c420381de
Carl Burks <crb02005@gmail.com>
2018-09-20 18:54:44 -0500

    Sync Serial Input

--- ArduinoMaster/ArduinoMaster.ino
+++ ArduinoMaster/ArduinoMaster.ino
@@ -3,14 +3,51 @@
 // Data L Out
 // CLK    Out
 
-
+//A0 Rotor speed
+//A1 effects intensity
+//A2 Volume
   
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, INPUT);          
-  pinMode(1, OUTPUT);          
-  pinMode(2, OUTPUT);          
-  pinMode(3, OUTPUT);          
+  pinMode(0, INPUT);  // 100 hz interrupt          
+  pinMode(1, OUTPUT); // motor serial data R
+  pinMode(2, OUTPUT); // motor serial data L
+  pinMode(3, OUTPUT); // Motor serial clock
+
+  pinMode(14, OUTPUT); // Display Bit 0
+  pinMode(15, OUTPUT); // Display Bit 1
+  pinMode(16, OUTPUT); // Display Bit 2
+  pinMode(17, OUTPUT); // Display Bit 3
+  pinMode(18, OUTPUT); // Display Bit 4
+  pinMode(19, OUTPUT); // Display Bit 5
+  pinMode(20, OUTPUT); // Display Bit 6
+  pinMode(21, OUTPUT); // Display Bit 7
+
+  pinMode(25, INPUT);  // PATCH Up
+  pinMode(26, INPUT);  // PATCH Down
+
+  pinMode(30, OUTPUT); // DAC CS
+  pinMode(31, OUTPUT); // DAC CLK
+  pinMode(28, OUTPUT); // DAC LDAC
+
+
+  pinMode(48, OUTPUT); // Future DAC 
+  pinMode(49, OUTPUT);
+  pinMode(46, OUTPUT);
+  pinMode(47, OUTPUT);
+  pinMode(44, OUTPUT);
+  pinMode(45, OUTPUT);
+  pinMode(42, OUTPUT);
+  pinMode(43, OUTPUT);
+  pinMode(40, OUTPUT);
+  pinMode(41, OUTPUT);
+  pinMode(38, OUTPUT);
+  pinMode(39, OUTPUT);
+  pinMode(36, OUTPUT);
+  pinMode(37, OUTPUT);
+  pinMode(34, OUTPUT);
+  pinMode(35, OUTPUT);
+
 }
 
 void multiShiftOut(int pins,int values){
--- newSpeaker/newSpeaker.ino
+++ newSpeaker/newSpeaker.ino
@@ -1,9 +1,84 @@
+int incomingByte = 0;   // for incoming serial data
+
+// 0 CLK
+// 1 Data // PORT 21
 void setup() {
-  // put your setup code here, to run once:
+        pinMode(0,INPUT);
+        pinMode(1,INPUT);
+        Serial.begin(115200); // Serial 0 is computer
+//        Serial1.begin(115200); // Serial 1 is ethernet cable
+}
+
+//https://www.arduino.cc/en/Hacking/PinMapping32u4
+//readClock
+//speedRead(PORTD,2);
+//readData
+//speedRead(PORTD,3);
+
+//#define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_input_PGM + (P))) )
+
+#define wordEnhanced(addr)         \
+(__extension__({                            \
+    uint16_t __addr16 = (uint16_t)(addr);   \
+    uint16_t __result;                      \
+    __asm__                                 \
+    (                                       \
+        "lpm %A0, Z+"   "\n\t"              \
+        "lpm %B0, Z"    "\n\t"              \
+        : "=r" (__result), "=z" (__addr16)  \
+        : "1" (__addr16)                    \
+    );                                      \
+    __result;                               \
+}))
+
+int speedRead(uint8_t port, uint8_t bit)
+{
+//  if (wordEnhanced( port_to_input_PGM + (port)) & bit) return HIGH;
+  if (*portInputRegister(port) & bit) return HIGH;
+  return LOW;
+}
+
+int digitalReadNoPWM(uint8_t pin)
+{
+  uint8_t bit = digitalPinToBitMask(pin);
+  uint8_t port = digitalPinToPort(pin);
+  if (*portInputRegister(port) & bit) return HIGH;
+  return LOW;
+}
+
+
+uint16_t shiftIn16(uint16_t dataPin, uint16_t clockPin) {
+
+  uint16_t value = 0;
+  
+  uint16_t i;
+  
+  for (i = 0; i < 16; ++i) {
+    //Check the clock
+//    while(!speedRead(PORTD,2)){
+//    }  
+//    value |= speedRead(PORTD,3) << (15 - i);
+      while(!digitalRead(0)){
+      }
+      while(digitalRead(0)){
+      }
+      value |= digitalRead(1) << (15 - i);
+  }
+  
+  return value;
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+        // send data only when you receive data:
+                // read the incoming byte:
+  //              incomingByte = Serial1.read();
+       //         if(!digitalRead(0)){
+                  incomingByte = shiftIn16(1, 0);
+                  // say what you got:
+                  Serial.println(incomingByte, HEX);
+         //       }
+         delay(1000);
 }
+ 

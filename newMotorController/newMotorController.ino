1d5b88e22ce65c1c979b08d9dc91cd55ab230216
Greg Fundyler <gfundyler@gmail.com>
2019-03-04 21:30:50 -0600

    Got SPI receive working on slave

--- newMotorController/newMotorController.ino
+++ newMotorController/newMotorController.ino
@@ -11,12 +11,14 @@ void setup() {
   // have to send on master in, *slave out*
 
   //pinMode(MOSI, INPUT);
+
+  //delay(1000);
   
   // turn on SPI in slave mode
   spi_enable();
 
   // turn on interrupts
-  SPCR |= bit(SPIE);
+  //SPCR |= bit(SPIE);
 
   //TODO: setup correct SPI phase and polarity to match DACs
   
@@ -24,29 +26,40 @@ void setup() {
   //https://gist.github.com/argon/4578068
   
   sck_detector_init();
+
+  while(!Serial){
+      ; // waiting for connection
+  }
+
+  Serial.println("Up and running");
+
+  TCCR1A = 0;
+  TCCR1C = 0;
+  rxtimer_stop();       // initialize timer
 }
 
 
 // ------------------------------ Receive Timer ------------------------------
-/* Using Timer/Counter0 for SPI timeout(8-bit timer)
+/* Using Timer/Counter1 for SPI timeout(8-bit timer)
  *  It takes about 32 us to receive a byte over SPI.
- *  This timer configured for clk/8 yields 128 us for a timer overflow(counting from 0 to 255)
- *  at 16 MHz system clock. Even clk/64 yielding 1.024 ms may be fine given the low update rate.
+ *  This timer configured for clk/8 yields 128 us for a timer overflow(counting from -256 to 0)
+ *  at 16 MHz system clock.
  */
 
 // has no effect if already started(continues running toward timeout)
 static void rxtimer_start(void) {
-  TCCR0B = 2;               // counter running at clk/8
+  TCCR1B = bit(CS11);       // counter running at clk/8 (2 MHz)       FIXME
 }
 
 static void rxtimer_stop(void) {
-  TCCR0B = 0;               // counter not running
-  TCNT0 = 0;                // reset counter
-  TIFR0 = bit(TOV0);        // reset overflow flag
+  TCCR1B = 0;               // counter not running
+  TCNT1H = 0xFF;            // reset counter to -256
+  TCNT1L = 0x00;
+  TIFR1 = bit(TOV1);        // reset overflow flag
 }
 
 static bool rxtimer_timeout(void) {
-  return TIFR0 & bit(TOV0);
+  return (TIFR1 & bit(TOV1)) == bit(TOV1);
 }
 
 
@@ -72,11 +85,11 @@ volatile uint16_t rx_word = 0;
 volatile int rx_byte_count = 0;
 
 static void spi_enable(void) {
-  SPCR |= bit(SPE);
+  SPCR = bit(SPE) | bit(SPIE);
 }
 
 static void spi_disable(void) {
-  SPCR &= ~bit(SPE);
+  SPCR = 0; //&= ~(bit(SPE) | bit(SPIE));
 }
 
 // SPI interrupt routine
@@ -98,21 +111,25 @@ static void rx_reset(void) {
   spi_enable();
 }
 
+
 void loop(void) {
   
   //Serial.println(SPDR);
 
   if(rx_byte_count == 2) {
-    rx_reset();
-
     // TODO: do something useful with rx_word
     //buf[pos] = 0;  
+    //delay(100);
     Serial.println(rx_word);
-    //pos = 0;
-
-  } else if(rxtimer_timeout || rx_byte_count > 2) {
     rx_reset();
-  } else if(sck_detected) {
+
+
+  } else if(rxtimer_timeout() || rx_byte_count > 2) {
+    if(rxtimer_timeout()) {
+      Serial.println("timeout");
+    }
+    rx_reset();
+  } else if(sck_detected()) {
     rxtimer_start();
     //sck_detector_reset();     // TODO: may be unnecessary
   }
